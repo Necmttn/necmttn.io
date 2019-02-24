48bdf75b76411f820623f24339f3a80234cfd1c5
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2019-02-24 16:49:26 +0700

    update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -137,7 +137,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-115374727-1',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
